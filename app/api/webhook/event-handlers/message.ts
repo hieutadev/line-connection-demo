9bcbdbb54a731d3063b03330ee6bcce723ae3038
Hieu Ta <hieu.ta@executionlab.asia>
2026-04-20 14:44:19 +0700

    feat(webhook): enhance message event handling with AI response generation

--- app/api/webhook/event-handlers/message.ts
+++ app/api/webhook/event-handlers/message.ts
@@ -1,12 +1,22 @@
 import lineBotClient from "@/lib/line-bot";
 import { MessageEvent } from "@/types/webhook-events";
+import { HumanMessage } from "@langchain/core/messages";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const model = new ChatGoogleGenerativeAI({ model: "gemini-2.5-flash" });
 
 export default async function handleMessageEvent(event: MessageEvent) {
   switch (event.message.type) {
     case "text":
+      const input = [
+        new HumanMessage({
+          content: [{ type: "text", text: event.message.text }],
+        }),
+      ];
+      const res = await model.invoke(input);
       await lineBotClient.replyMessage({
         replyToken: event.replyToken,
-        messages: [{ type: "text", text: "Hello, you just send a message 👋🏼" }],
+        messages: [{ type: "text", text: res.content.toString() }],
       });
       break;
     case "image":
